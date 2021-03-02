b0cedaf05c37637652ee60755c70f59e6f3b0ab5
Carlos Oliveira <oliveiracarlos2001@hotmail.com>
2021-03-02 17:25:08 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,17 +2,17 @@ const { Client, MessageEmbed } = require('discord.js');
 const bot = new Client();
 bot.once('ready', () => {console.log(`Bot Online e operante`)});
 
-
 bot.on('message', msg => {
     if (msg.content === '!oi') {
         const embed = new MessageEmbed()
             .setTitle('Olá!!')
-            .setColor(7419530)
-            .setDescription('Eu ainda estou em desenvolvimento, mas em breve vou poder alegrar vocês!')
-            .setFooter('Prodd neste momento')
-            .setImage('https://media.giphy.com/media/LmNwrBhejkK9EFP504/giphy.gif');    
+            .setColor(0000000)
+            .setImage('https://media.giphy.com/media/LmNwrBhejkK9EFP504/giphy.gif')
+            .addField('Sou o bot do Survidor','Eu ainda estou em desenvolvimento, mas em breve vou poder entreter vocês!')
+            .setFooter('Imagens do Prodd neste momento'); 
+            msg.channel.send(embed);
+            msg.delete({timeout: 1000});  
         }   
-        
     }            
 );
 
