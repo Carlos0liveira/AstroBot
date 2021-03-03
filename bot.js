c3ed6056b047845a7701b0aaa951221eb7a3f3f0
Carlos Oliveira <43425597+Carlos0liveira@users.noreply.github.com>
2021-03-02 22:02:29 -0300

    Bot.js

--- bot.js
+++ bot.js
@@ -1,19 +1,31 @@
 const { Client, MessageEmbed } = require('discord.js'); 
 const bot = new Client();
-bot.once('ready', () => {console.log(`Bot Online e operante`)});
+const cfg = require("./config.json");
 
-bot.on('message', msg => {
-    if (msg.content === '!oi') {
+bot.on('ready', () => {
+    console.log(`Bot Online e operante`)
+    bot.user.setActivity(`Sou o Bot do Surv digite !ajuda para ver meus comandos`)
+    }
+);
+
+bot.on('message', message => {
+    if (message.author.bot) return;
+    if (message.channel.type === "dm") return; 
+        
+    const arg = message.content.slice(cfg.prefix.length).trim().split(/ +/g);
+    const command = arg.shift().toLowerCase();
+
+    if (command === 'oi') {
         const embed = new MessageEmbed()
             .setTitle('Olá!!')
             .setColor(0000000)
             .setImage('https://media.giphy.com/media/LmNwrBhejkK9EFP504/giphy.gif')
             .addField('Sou o bot do Survidor','Eu ainda estou em desenvolvimento, mas em breve vou poder entreter vocês!')
             .setFooter('Imagens do Prodd neste momento'); 
-            msg.channel.send(embed);
-            msg.delete({timeout: 1000});  
+            message.channel.send(embed);
+            message.delete({timeout: 1000});  
         }   
     }            
 );
 
-  bot.login('ODE2MDQyNDg5Mjk1Mjc0MDQ1.YD1Myg.n6y8E2RqlPQhwkTRfCStKvmjDjw')
+  bot.login(cfg.token)
