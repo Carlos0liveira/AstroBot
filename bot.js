0f2bc3b1334da844111ed3daaf63519bd4469e22
Carlos Oliveira <43425597+Carlos0liveira@users.noreply.github.com>
2021-03-03 20:46:20 -0300

    SQLITE3

--- bot.js
+++ bot.js
@@ -5,6 +5,9 @@ const saudacao = require('./src/comands/saudacao');
 const ajuda = require('./src/comands/ajuda')
 const joinServer = require('./src/joinServer')
 
+const sqlite3 = require('sqlite3').verbose();
+let db = new sqlite3.Database('./AstroDB.db');
+
 bot.on('ready', () => {
     console.log(`Bot Online e operante`)
     bot.user.setActivity(`Prazer me chamo Astro digite !ajuda para ver meus comandos`);
