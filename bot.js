3578a213e84f68df91bf04223e3c67aba4824ab8
Carlos Oliveira <oliveiracarlos2001@hotmail.com>
2021-03-03 14:46:07 -0300

    Index do Projeto
    Adicionada a chamada de funções e subdividido o código.

--- bot.js
+++ bot.js
@@ -1,6 +1,8 @@
-const { Client, MessageEmbed } = require('discord.js'); 
+const { Client } = require('discord.js'); 
 const bot = new Client();
 const cfg = require("./config.json");
+const saudacao = require('./src/comands/saudação');
+const ajuda = require('./src/comands/ajuda')
 
 bot.on('ready', () => {
     console.log(`Bot Online e operante`)
@@ -8,21 +10,11 @@ bot.on('ready', () => {
     }
 );
 
-bot.on('message', message => {
+bot.on('message', (message) => {
     if (message.author.bot) return;
-    if (message.channel.type === "dm") return;
+    if (message.channel.type === "dm") return;    
+    saudacao(message);
+    ajuda(message);
+});
 
-    if (message.content === '!oi') {
-        const embed = new MessageEmbed()
-            .setTitle('Olá! Me chamo Astro')
-            .setColor([45,25,52])
-            .setImage('https://media.giphy.com/media/LmNwrBhejkK9EFP504/giphy.gif')
-            .addField('Sou o bot do Survidor','Eu ainda estou em desenvolvimento, mas em breve vou poder entreter vocês!')
-            .setFooter('Imagens do Prodd neste momento'); 
-            message.channel.send(embed);
-            message.delete({timeout: 500});  
-        }   
-    }            
-);
-
-  bot.login(cfg.token)
+bot.login(cfg.token)
